38167033f988a272e0f806dcc3e909fb9155e021
Marty Lamb <mlamb@martiansoftware.com>
2016-10-01 09:04:15 -0400

    pom formatting

--- src/main/java/com/martiansoftware/validation/Validator.java
+++ src/main/java/com/martiansoftware/validation/Validator.java
@@ -1,5 +1,19 @@
 package com.martiansoftware.validation;
 
+//   Copyright 2016 Martian Software, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -10,11 +24,16 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- *
+ * TODO class documentation
+ * TODO licenses
+ * TODO readme
+ * 
  * @author mlamb
+ * @param <T> the type of value to validate
+ * @param <E> the type of exception to throw if validation fails
  */
 public class Validator <T, E extends Exception> {
-    
+
     /**
      * the value that is being validated
      */
@@ -110,8 +129,7 @@ public class Validator <T, E extends Exception> {
     public Validator <T, E> isNotPresent() throws E {
         if (_isPresent()) invalid("%s must not be present", _name);
         return this;
-    }
-    
+    }    
     
     /**
      * Validates that the value is not null or empty.  Emptiness
